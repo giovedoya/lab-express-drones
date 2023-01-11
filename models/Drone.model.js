d0f37a4b153b3bd117e53986cdf7632e1ad4bffd
Giovanni Bedoya <giovedoya@gmail.com>
2023-01-11 20:57:32 +0100

    'Iteration 1 | Seed the database'

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,11 @@
-// Iteration #1
+const mongoose = require('mongoose');
+const { Schema }= mongoose;
+
+const droneSchema = new Schema ({
+    name: String,
+    propellers: Number,
+    maxSpeed: Number
+})
+const Drones = mongoose.model('Drones', droneSchema);
+
+module.exports = Drones;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,19 @@
-// Iteration #1
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true)
+const Movie = require('../models/Drone.model');
+const MONGO_URL = 'mongodb://127.0.0.1:27017/lab-express-drone'; 
+
+mongoose.connect(MONGO_URL)
+  .then(response => console.log(`Connected to the database ${response.connection.name}`))
+  .then (() => {
+      Movie.create(drones)
+  })
+  .then(createdDrones => console.log(`Inserted ${createdDrones.length} drones in the database`))
+  .then(() => mongoose.connection.close())
+  .catch(err => console.log(err))
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
